858ca283befc67d87684e44945b020c9e62ebaad
killbyte <varkillbyte@gmail.com>
2026-06-10 02:30:28 +0300

    feat: add rules in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,18 @@ export default defineConfig([
     },
     rules: {
       semi: 'off',
-    },
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      'no-console': 'warn',
+    }
   },
 ])
