7ebd8567cb10e4ee5a0816d354cda5e3201a13e6
killbyte <varkillbyte@gmail.com>
2026-06-16 19:28:45 +0300

    feat: add vitest config and run script vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
+// позволяет TypeScript получить корректную типизацию для Vitest
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Включает глобальные функции из Vitest
+    environment: 'jsdom', // Устанавливает окружение для тестов как 'jsdom', имитирующую браузер
+  },
 })
